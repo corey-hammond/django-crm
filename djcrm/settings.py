591f5ba8de5bb85b79f8eb891624a82757e5afbf
Corey Hammond <coreyjhammond@gmail.com>
2021-01-29 23:02:10 -0800

    .env changes

--- djcrm/settings.py
+++ djcrm/settings.py
@@ -14,7 +14,7 @@ environ.Env.read_env()
 
 DEBUG = env('DEBUG')
 
-SECRET_KEY = env('SECRET_KEY', 'sample_secret')
+SECRET_KEY = env('SECRET_KEY')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
